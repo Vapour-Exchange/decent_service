8d6dbfea87336b16fbc03556c455673123ae7d02
Abhishek goel <abhishek.goel@truereach.ai>
2024-05-29 05:21:41 +0530

    quote api

--- routes/uniSwap.js
+++ routes/uniSwap.js
@@ -193,4 +193,87 @@ router.post("/approve-max", async (req, res) => {
   }
 });
 
+router.get("/quote", async (req, res) => {
+  logger.info("Request body:", req.body);
+
+  try {
+    const {
+      tokenInput: {
+        address: tokenInputAddress,
+        decimal: tokenInputDecimal,
+        symbol: tokenInputSymbol,
+        name: tokenInputName,
+        amount: tokenInputAmount,
+      },
+      tokenOutput: {
+        address: tokenOutputAddress,
+        decimal: tokenOutputDecimal,
+        symbol: tokenOutputSymbol,
+        name: tokenOutputName,
+      },
+    } = req.body;
+
+    const { inputToken, outputToken } = createTokens(
+      config.chainId,
+      tokenInputAddress,
+      tokenInputDecimal,
+      tokenInputSymbol,
+      tokenInputName,
+      tokenOutputAddress,
+      tokenOutputDecimal,
+      tokenOutputSymbol,
+      tokenOutputName
+    );
+
+    logger.info(
+      `Tokens Created: ,
+      ${JSON.stringify(inputToken)},
+      ${JSON.stringify(outputToken)}`
+    );
+
+    const inputAmountInWei = createAmountInWei(
+      tokenInputAmount.toString(),
+      tokenInputDecimal
+    );
+
+    logger.info(`Input Amount in Wei ${inputAmountInWei}`);
+
+    const wethAmount = CurrencyAmount.fromRawAmount(
+      inputToken,
+      JSBI.BigInt(inputAmountInWei.toString())
+    );
+    logger.info(`Input Amount Parsed in Weth ${wethAmount}`);
+
+    const options = getSwapOptions(config.walletAddress);
+
+    logger.info(`Swap Router options ${JSON.stringify(options)}`);
+
+    const alphaRouter = getRouter(config.chainId, provider);
+
+    logger.info(`Aplha Router initialized`);
+
+    const route = await alphaRouter.route(
+      wethAmount,
+      outputToken,
+      TradeType.EXACT_INPUT,
+      options
+    );
+
+    if (!route) {
+      throw new Error("No route found for the swap");
+    }
+
+    logger.info(`Route found, ${JSON.stringify(route.quote.toFixed(10))}`);
+
+    res
+      .status(200)
+      .json({ success: true, data: { amount: route.quote.toFixed(10) } });
+  } catch (error) {
+    logger.error("Error in swap process:", error);
+    res
+      .status(500)
+      .json({ success: false, error: "Failed to execute the swap" });
+  }
+});
+
 export default router;
